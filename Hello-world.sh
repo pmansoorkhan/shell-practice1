682cf6d53073607b7d2a8c0d824d06eff5f669ce
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-15 06:14:40 +0530

    modified

--- Hello-world.sh
+++ Hello-world.sh
@@ -3,3 +3,5 @@
 # this is for writing information about the code, which is non executable and only for our understanding
 echo "Hello World!"
 echo "Welcome to the Git learning session" 
+echo "Welcome to equitas project"
+
