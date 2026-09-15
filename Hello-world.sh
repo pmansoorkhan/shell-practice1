d09573f5ac3e706ccdc616ceceb36c452443eee8
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-15 05:50:21 +0530

    modifed hello-world

--- Hello-world.sh
+++ Hello-world.sh
@@ -2,4 +2,4 @@
 
 # this is for writing information about the code, which is non executable and only for our understanding
 echo "Hello World!"
-echo "Welcome to the Git course" 
+echo "Welcome to the Git learning session" 
