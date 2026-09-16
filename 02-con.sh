4c0729c174ee657cc0b0068e45ddebaa2b9d3612
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-16 06:05:16 +0530

    varaiables class

--- 02-con.sh
+++ 02-con.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Mansoor::Hi Ramesh, How are you ?"
-echo "Ramesh:: Hi Mansoor, I am fine, what about you?"
-echo "Mansoor:: I am fine Ramesh, Thanks for asking, what are you doing now Ramesh?"
-echo "Ramesh:: Ya Mansoor, I am learning new course as shell scripting for my internal project"
-echo "Mansoor:: Good Ramesh, We are also moving towards cloud and I too started learning some technical skills, Ramesh" 
-echo "Ramesh:: Bye Mansoor, Will connect on coming sunday"
+echo"Mansoor::Hi Ramesh, How are you ?"
+echo"Ramesh:: Hi Mansoor, I am fine, what about you?"
+echo"Mansoor:: I am fine Ramesh, Thanks for asking, what are you doing now Ramesh?"
+echo"Ramesh:: Ya Mansoor, I am learning new course as shell scripting for my internal project"
+echo"Mansoor:: Good Ramesh, We are also moving towards cloud and I too started learning some technical skills, Ramesh" 
+echo"Ramesh:: Bye Mansoor, Will connect on coming sunday"
