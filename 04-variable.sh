9037e0dab98434ea6b267ab6118ef13220789a96
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-21 08:15:48 +0530

    read command

--- 04-variable.sh
+++ 04-variable.sh
@@ -2,7 +2,7 @@
 
 echo "Please enter your name::"
 read Name  # read will store the name value in it and gives the output when it  printed
-echo "your name is $Name"
 echo "please enter your mobile number::"
 read Mobile_number  # read will store the moble number value in it and gives the output when it  printed
+echo "your name is $Name"
 echo "your mobile number is $Mobile_number" 
