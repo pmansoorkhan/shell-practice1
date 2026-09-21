bec7867bc0c4576ed55ee2f0e42c498aa0666ed5
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-21 06:44:29 +0530

    redded new file

---
+++ 02-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2, How are you ?"
+echo "$PERSON2:: Hi $PERSON1, I am fine, what about you?"
+echo "$PERSON1:: I am fine $PERSON2, Thanks for asking, what are you doing now $PERSON2?"
+echo "$PERSON2:: Ya $PERSON1, I am learning new course as shell scripting for my internal project"
+echo "$PERSON1:: Good $PERSON2, We are also moving towards cloud and I too started learning some technical skills, Ramesh" 
+echo "$PERSON2:: Bye $PERSON1, Will connect on coming sunday"
