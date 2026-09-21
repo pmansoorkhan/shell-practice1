d883ae7b2595449f639fb81020f6f6a8d032c1e9
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-21 08:19:42 +0530

    read varaible

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# "read" is a  dynamic variable which stores the data and gives the output whenever it is printed. 
 echo "Please enter your name::"
 read Name  # read will store the name value in it and gives the output when it  printed
 echo "please enter your mobile number::"
