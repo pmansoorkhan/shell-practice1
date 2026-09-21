b8f50c0a78ad4664eaccdf92d8dcbc7015c703a0
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-21 09:45:39 +0530

    comment

--- 06-variable.sh
+++ 06-variable.sh
@@ -4,7 +4,7 @@ echo "please enter your name:"
 read Name
 
 echo "please enter your passowrd:"
-read -s Password  # "-s" denoted to keep the password value to be hidden 
+read -s Password  # "-s" denoted to keep the password value to be hidden while giving 
 
 echo "your name is $Name"
-echo "your password is $Password"
+echo "your password is $Password" # password will not be printed anywhere in production uses.
