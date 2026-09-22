5fa09426e56e6c6bfa92e88fd0d424f1e71b748f
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-22 08:06:06 +0530

    variable-timestamp

--- 08-variable.sh
+++ 08-variable.sh
@@ -4,5 +4,5 @@ Timestamp=$(date)
 echo "current date and time is: $Timestamp"
 
 
-log3=$(cat /etc/passwd  | cut -d ":" -f1, f2, f3, f4, f5, f6, f7)
+log3=$(cat /etc/passwd  | cut -d ":" -f1)
 echo "$log3"
