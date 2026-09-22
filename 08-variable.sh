c977b1d568ccbce92985b95fa106d0d82848347f
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-22 08:48:57 +0530

    variable-timestamp

--- 08-variable.sh
+++ 08-variable.sh
@@ -9,8 +9,8 @@ sleep 15
 
 endtime=$(date +%s)
 
-total_time=$(($endtime - $starttime))
-echo "total timetaken:$(total_time) seconds"
+totaltime=$(($endtime - $starttime))
+echo "total timetaken:$(totaltime) seconds"
 
 
 log3=$(cat /etc/passwd  | cut -d ":" -f1,3)
