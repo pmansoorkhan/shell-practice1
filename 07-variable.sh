453d483e39995a2821c56e4a794fac7c3b024536
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-22 06:52:31 +0530

    passing command output into a variable

--- 07-variable.sh
+++ 07-variable.sh
@@ -11,5 +11,8 @@ echo "Current date: $current_date"
 disk_usage=$(df -h / | tail -5)
 echo "$disk_usage"
 
-  logs=$(tail -10 /var/log/messages)
-  echo "$logs"
+logs=$(tail -10 /var/log/messages)
+echo "$logs"
+
+logs1=$(head -5 /etc/passwd)
+echo "$logs1"
