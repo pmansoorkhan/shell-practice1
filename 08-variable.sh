8376b71f111b6ac6b4c4701c528e2b8c3708cb8d
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-22 09:21:42 +0530

    variable-timestamp

--- 08-variable.sh
+++ 08-variable.sh
@@ -5,11 +5,13 @@ echo "current date and time is: $Timestamp"
 
 
 Starttime=$(date +%s)
-sleep 15
+sleep 12
 Endtime=$(date +%s)
 Totaltime=$(($Endtime -$Starttime))
 echo "Total time taken: $Totaltime seconds"
 
+echo "script starttime:$Starttime seconds"
+echo "script endtime: $Endtime seconds"
 
 log3=$(cat /etc/passwd  | cut -d ":" -f1,3)
 echo "$log3"
