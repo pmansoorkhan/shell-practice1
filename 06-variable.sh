6e6e5304170c1bbfbc9012c8a0d2fd75705d6472
pmansoorkhan <pmansoorkhan92@gmail.com>
2026-09-22 06:25:02 +0530

    updated few fields

--- 06-variable.sh
+++ 06-variable.sh
@@ -6,5 +6,9 @@ read Name
 echo "please enter your passowrd:"
 read -s Password  # "-s" denoted to keep the password value to be hidden while giving 
 
+echo "please enter your mobile number:"
+read Mobile_Number
+
 echo "your name is $Name"
 echo "your password is $Password" # password will not be printed anywhere in production uses.
+echo "your mobile number is $Mobile_Number"
